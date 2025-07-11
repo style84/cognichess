5ce4aeb1eebef7aa6dec837b268c41eb5de3c395
Ingo Krause <ingo@chemingo.de>
2025-07-11 16:16:38 +0200

    added first structs and const for the board

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,1 +1,24 @@
-package internal
+package board
+
+type Color int
+type PieceType int
+
+const (
+	White Color = iota
+	Black
+)
+
+const (
+	Pawn PieceType = iota
+	Knight
+	Bishop
+	Rook
+	Queen
+	King
+)
+
+type Piece struct {
+	Kind     PieceType
+	HasMoved bool
+	Player   Color
+}
