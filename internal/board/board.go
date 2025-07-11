e8105da55606fd0777e3db63bf5471601c0dbd8c
Ingo Krause <ingo@chemingo.de>
2025-07-11 22:30:28 +0200

    started the starpos-func

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,15 +1,16 @@
 package board
 
-type Color int
+type ColorType int
 type PieceType int
 
 const (
-	White Color = iota
+	White ColorType = iota
 	Black
 )
 
 const (
-	Pawn PieceType = iota
+	None PieceType = iota
+	Pawn
 	Knight
 	Bishop
 	Rook
@@ -20,5 +21,36 @@ const (
 type Piece struct {
 	Kind     PieceType
 	HasMoved bool
-	Player   Color
+	Player   ColorType
+}
+
+type Square struct {
+	OccupiedBy Piece
+	EnPassant  bool
+	IsValid    bool
+}
+
+func (s Square) IsEmpty() bool {
+	if s.OccupiedBy.Kind == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func CreateStartpos() []Square {
+	board := make([]Square, 120)
+
+	// Setting up white Pawns
+	for i := 21; i < 29; i++ {
+		board[i].OccupiedBy.Kind = Pawn
+		board[i].OccupiedBy.Player = White
+	}
+	// Setting up black Pawns
+	for i := 81; i < 89; i++ {
+		board[i].OccupiedBy.Kind = Pawn
+		board[i].OccupiedBy.Player = Black
+	}
+
+	return board
 }
