864a188bb96852db26d88f9ce4fabd76e3aae7f4
Ingo Krause <ingo@chemingo.de>
2025-07-12 22:37:11 +0200

    board has invalid squares

--- internal/board/board.go
+++ internal/board/board.go
@@ -38,9 +38,20 @@ func (s Square) IsEmpty() bool {
 	}
 }
 
+// CreateStartpos returns an array of 120 squares, representing a 1D-array for the board state
+// The game board is sourrounded by squares wich are invalid. This is needed for move generation to chech,
+// if a piece moves out of bounds
+//
+// The invalid squares are the lowest ang highest two rows [0]-[19] & [100]-[119]
+// as well as the left and rightmost files.
 func CreateStartpos() []Square {
 	board := make([]Square, 120)
-
+	// Setting the valid squares; invalid ones are automatically invalid (zero value of bool is false)
+	for i := 21; i < 98; i++ {
+		if i%10 != 0 && i%10 != 9 {
+			board[i].IsValid = true
+		}
+	}
 	// Setting up white Pawns
 	for i := 21; i < 29; i++ {
 		board[i].OccupiedBy.Kind = Pawn
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/style84/cognichess/internal/board"
 )
 
 func main() {
 	fmt.Println("Corgnichess is a minimal chess engine written in GO by Ingo Krause.")
+
+	var mailbox []board.Square = board.CreateStartpos()
+
+	for i, sq := range mailbox {
+		fmt.Println("Square", i, "is valid =", sq.IsValid)
+	}
 }
