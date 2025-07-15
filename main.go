8ff33a16e62621e0c36624cf487de0549f6f1f72
Ingo Krause <ingo@chemingo.de>
2025-07-15 15:31:38 +0200

    setting up board from FEN implemented

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	fmt.Println("Corgnichess is a minimal chess engine written in GO by Ingo Krause.")
 
-	var mailbox []board.Square = board.CreateStartpos()
+	var positionBoard []board.Square = board.CreateStartpos()
 
-	for i, sq := range mailbox {
+	for i, sq := range positionBoard {
 		fmt.Println("Square", i, "is valid =", sq.IsValid)
 	}
 }
